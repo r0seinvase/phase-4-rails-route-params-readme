951e8b02fbcedfb8d102a263f5a53aaf537f59cf
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-04-25 22:55:11 -0500

    commit tree e3c48ff516d80694e1cf72a126bc68dcd8d28370

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show'
 end
+
+
+# Rails.application.routes.draw do
+#   get '/cheeses', to: 'cheeses#index'
+#   get '/cheeses/:id', to: 'cheeses#show'
+# end
